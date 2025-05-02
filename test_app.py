25f45316660df5cacd653d8e7072c72fdd9e06a5
diganthdr <diganth@gmail.com>
2025-05-02 15:33:57 +0530

    fix test

--- test_app.py
+++ test_app.py
@@ -18,5 +18,5 @@ def test_generate_route(client):
     data = response.get_json()
     assert 'number' in data
     assert isinstance(data['number'], int)
-    assert 10000 <= data['number'] <= 100000
+    assert 100000 <= data['number'] <= 1000000
     # assert 1 <= data['number'] <= 100 
